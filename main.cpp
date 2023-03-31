efde5cfd39eefb208814f5ee6dc3a0f87f53b09c
Galang  Satrio Wicaksono <galang0559@gmail.com>
2023-03-31 18:04:09 +0700

    update313_1804

--- main.cpp
+++ main.cpp
@@ -2,16 +2,35 @@
 
 using namespace std;
 
+/*
+ method buat ngitung keluaran literannya misal nominal/10000 buat pertalite, nominal/12500 pertamax.
+ perulangan buat reset mesing, keseluruhan diulang
+
+
+*/
 class penghitungan{
 public:
-    int keluarpertalite(int nominal);
-    return nominal/10000;
+    double pertalet(double uang){
+    return uang/10000;
+    }
 };
-
 int main()
 {
-    int jenis, liter, repeat, harga;
-        penghitungan itung;
+    penghitungan hitung;
+    int jenis, repeat;
+    double liter, harga, nominal;
+    cin>>jenis;
+    switch(jenis){
+        case 1:
+            cout<<"Mau beli seharga berapa? : \n";
+            cin>>nominal;
+            liter = hitung.pertalet(nominal);
+            cout<<"Anda membeli pertalet sebanyak "<< liter <<" liter";
+            break;
+        default:
+            cout<<"Masukkan jenis dengan benar!";
+            break;
+        }
 
     return 0;
 }
